06b92c4e77e167ac1777d3870b71d8fc7edea787
theaccountoflhy <luohongyan511@163.com>
2021-03-31 19:31:33 +0800

    笔记本修改

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hallow!");
+        System.out.println("Hallo! Java!");
+        System.out.println("Hallo! Github!");
     }
 }
