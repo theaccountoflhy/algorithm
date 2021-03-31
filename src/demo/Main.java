7f4693ebe8bff56ffd9123d20e455f8c6a5cfc6c
luohongyan <luohongyan511@163.com>
2021-03-31 17:30:42 +0800

    first commit all

---
+++ src/demo/Main.java
@@ -0,0 +1,12 @@
+package demo;
+
+import java.util.Scanner;
+
+/**
+ * Create by hongyan on 2021/3/31
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hallow!");
+    }
+}
