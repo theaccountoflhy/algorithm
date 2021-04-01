67c1b74266f2217d60d07b15fb7544234957cf34
luohongyan <luohongyan511@163.com>
2021-04-01 18:08:30 +0800

    0401

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,12 +1,18 @@
 package demo;
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Create by hongyan on 2021/3/31
  */
-//public class Main {
-//    public static void main(String[] args){
-//        System.out.println("Hallo! Java!");
-//        System.out.println("Hallo! Github!");
-//    }
-//}
+public class Main {
+    public static void main(String[] args){
+        String to = "luohongyan";
+        char[] luo = new char[to.length()];
+        luo=to.toCharArray();
+        Arrays.sort(luo);
+        System.out.println(luo[0]);
+    }
+}
